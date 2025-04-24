45517250ff7bc5feb9630a7f8907fe1766eafb78
Angela Fisher <ajf-noreply@phantom-mail.io>
2025-04-23 17:54:15 -0700

    Missed a fix

--- backend/server.ts
+++ backend/server.ts
@@ -10,7 +10,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 
 import { RegisterRoutes } from "./routes/routes";
-import { Errors } from "./types/index";
+import { Errors } from "./types";
 
 const app = express();
 
